85b989280a2c0249cbbe6541a83dbaa82ade87e7
Braxton Eddings <beddings81@gmail.com>
2023-02-07 13:36:02 -0600

    Add sad path test for items index

--- spec/request/api/v1/items_request_spec.rb
+++ spec/request/api/v1/items_request_spec.rb
@@ -2,37 +2,57 @@ require 'rails_helper'
 
 describe 'Items API', type: :request do
   describe 'Items index' do
-    it 'sends a list of items' do
-      create_list(:item, 4)
-    
-      get '/api/v1/items'
+    describe 'happy path' do
+      it 'sends a list of items' do
+        create_list(:item, 4)
       
-      expect(response).to be_successful
-      
-      items = JSON.parse(response.body, symbolize_names: true)
+        get '/api/v1/items'
+        
+        expect(response).to be_successful
+        
+        items = JSON.parse(response.body, symbolize_names: true)
 
-      expect(items).to have_key(:data)
-      expect(items[:data]).to be_a(Array)
-      expect(items[:data].count).to eq(4)
+        expect(items).to have_key(:data)
+        expect(items[:data]).to be_a(Array)
+        expect(items[:data].count).to eq(4)
 
-      items[:data].each do |item|
-        expect(item).to have_key(:id)
-        expect(item[:id]).to be_an(String)
+        items[:data].each do |item|
+          expect(item).to have_key(:id)
+          expect(item[:id]).to be_an(String)
 
-        expect(item).to have_key(:attributes)
-        expect(item[:attributes]).to be_a(Hash)
+          expect(item).to have_key(:attributes)
+          expect(item[:attributes]).to be_a(Hash)
 
-        expect(item[:attributes]).to have_key(:name)
-        expect(item[:attributes][:name]).to be_a(String)
+          expect(item[:attributes]).to have_key(:name)
+          expect(item[:attributes][:name]).to be_a(String)
 
-        expect(item[:attributes]).to have_key(:description)
-        expect(item[:attributes][:description]).to be_a(String)
+          expect(item[:attributes]).to have_key(:description)
+          expect(item[:attributes][:description]).to be_a(String)
 
-        expect(item[:attributes]).to have_key(:unit_price)
-        expect(item[:attributes][:unit_price]).to be_a(Float)
+          expect(item[:attributes]).to have_key(:unit_price)
+          expect(item[:attributes][:unit_price]).to be_a(Float)
 
-        expect(item[:attributes]).to have_key(:merchant_id)
-        expect(item[:attributes][:merchant_id]).to be_a(Integer)
+          expect(item[:attributes]).to have_key(:merchant_id)
+          expect(item[:attributes][:merchant_id]).to be_a(Integer)
+        end
+      end
+    end
+
+    describe 'sad path' do
+      it 'returns an empty array when theres no items in the database' do
+        get '/api/v1/items'
+        
+        expect(response).to_not be_successful
+
+        items = JSON.parse(response.body, symbolize_names: true)
+
+        expect(items).to have_key(:message)
+        expect(items[:message]).to eq("The query could not be completed")
+
+        expect(items).to have_key(:errors)
+        expect(items[:errors]).to be_a(Array)
+        expect(items[:errors][0]).to eq("There are no items in the database")
+        
       end
     end
   end
