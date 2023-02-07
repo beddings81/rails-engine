049699c0d6bcabd36d635b9a11d648247d19c9fc
Braxton Eddings <beddings81@gmail.com>
2023-02-07 13:18:21 -0600

    Pass sad path test for item show

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,18 +1,27 @@
 class Api::V1::ItemsController < ApplicationController
   def index
     items = Item.all
-    
-    if params[:merchant_id]
-      merchant = Merchant.find(params[:merchant_id])
-      render json: ItemSerializer.new(merchant.items, merchant)
-    else
-      render json: ItemSerializer.new(items)
-    end
+    require 'pry'; binding.pry
+    # if items == nil
+    #   render :json => { :errors => Item.errors.full_messages }
+    # else
+    #merchant items
+      if params[:merchant_id]
+        merchant = Merchant.find(params[:merchant_id])
+        render json: ItemSerializer.new(merchant.items, merchant)
+    #items
+      else
+        render json: ItemSerializer.new(items)
+      end
+    # end
   end
 
   def show
-    item = Item.find(params[:id])
-
-    render json: ItemSerializer.new(item)
+    if Item.exists?(params[:id])
+      item = Item.find(params[:id])
+      render json: ItemSerializer.new(item)
+    else
+      render json: { "message": "your query could not be completed", :errors => ["Item does not exist"] }, status: 404
+    end
   end
 end
