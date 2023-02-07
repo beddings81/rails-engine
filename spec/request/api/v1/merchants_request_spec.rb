2e327557ace1a12c041de8ab501cb07bb023d46f
Braxton Eddings <beddings81@gmail.com>
2023-02-07 14:20:04 -0600

    Update happy path test for merchant index

--- spec/request/api/v1/merchants_request_spec.rb
+++ spec/request/api/v1/merchants_request_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe 'Merchant API', type: :request do
   describe 'merchants index' do
     describe 'happy path' do
-      it 'sends a list of items' do
+      it 'sends a list of merchants' do
         create_list(:merchant, 3)
 
         get '/api/v1/merchants'
