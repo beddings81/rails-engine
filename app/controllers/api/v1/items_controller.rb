3faceeb4b7537513a26fe4bcf15aa06258f56f71
Braxton Eddings <beddings81@gmail.com>
2023-02-07 02:04:15 -0600

    Pass test for merchant items endpoint

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,6 +1,12 @@
 class Api::V1::ItemsController < ApplicationController
   def index
     items = Item.all
-    render json: ItemSerializer.new(items)
+    
+    if params[:merchant_id]
+      merchant = Merchant.find(params[:merchant_id])
+      render json: ItemSerializer.new(merchant.items, merchant)
+    else
+      render json: ItemSerializer.new(items)
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :items, only: [:index]
-      resources :merchants, only: [:index, :show]
+      resources :merchants, only: [:index, :show] do
+        resources :items, only: [:index]
+      end
     end
   end
 end
