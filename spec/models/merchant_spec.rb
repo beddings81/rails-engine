f30dbc1381df2eb9a53df9915bf63f0d4ff48198
Braxton Eddings <beddings81@gmail.com>
2023-02-08 18:32:42 -0600

    Add merchant model test for #find_merchant_by_name

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -8,4 +8,15 @@ RSpec.describe Merchant, type: :model do
   describe 'associations' do
     it { should have_many(:items) }
   end
+
+  describe '#find_merchant_by_name' do
+    it 'will return one merchant based off the search' do
+      merchant1 = Merchant.create!(name: "Turing")
+      merchant2 = Merchant.create!(name: "Lord of the Ring")
+      merchant3 = Merchant.create!(name: "Ring Her")
+
+      expect(Merchant.find_merchant_by_name("ring")).to eq(merchant2)
+      expect(Merchant.find_merchant_by_name("z")).to eq(nil)
+    end
+  end
 end
